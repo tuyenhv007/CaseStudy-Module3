ab954f2812976a201153b6ba61c6d912de80fe03
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-07-07 15:31:00 +0700

    merge design pattern login-admin

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,29 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\LoginService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    protected $loginService;
+    public function __construct(LoginService $loginService)
+    {
+        $this->loginService = $loginService;
+    }
 
     public function formLogin()
     {
         return view('admin.login');
     }
 
-    public function login(Request $request)
+    public function login( Request $request)
     {
-        $user = [
-            'name' => $request->email,
-            'password' => $request->password
-        ];
-
-        if (Auth::attempt($user)) {
+        if (!$this->loginService->login($request)) {
             return back();
-        } else {
-            return view('admin.dashboard');
         }
+        return view('admin.dashboard');
     }
 
     public function logout()
