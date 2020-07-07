9552078811814454c215f239980185336b7f6b9d
Your Name <you@example.com>
2020-07-07 13:13:11 +0700

    remove update product in cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -27,6 +27,42 @@ class CartController extends Controller
         return back();
     }
     public function index(){
-        return view('cart/index');
+        $cart = Session::get('cart');
+        return view('cart.index', compact('cart'));
+    }
+    public function remove($idProduct)
+    {
+        if (Session::has('cart')) {
+            $oldCart = Session::get('cart');
+            if (count($oldCart->items) > 0) {
+                $cart = new Cart($oldCart);
+                $cart->remove($idProduct);
+                Session::put('cart', $cart);
+                toastr()->success('Đã xóa sản phẩm trong giỏ hàng', 'Success');
+            } else {
+                toastr()->error('Bạn chưa mua sp nào', 'Inconceivable!');
+            }
+        } else {
+            toastr()->error('Bạn chưa mua sp nào', 'Inconceivable!');
+        }
+        return back();
+    }
+
+    public function update(Request $request, $idProduct)
+    {
+        if (Session::has('cart')) {
+            $oldCart = Session::get('cart');
+            if (count($oldCart->items) > 0) {
+                $cart = new Cart($oldCart);
+                $cart->update($request, $idProduct);
+                Session::put('cart', $cart);
+                toastr()->success('Giỏ hàng vừa được cập nhật');
+            } else {
+                toastr()->error('fail', 'Inconceivable!');
+            }
+        } else {
+            toastr()->error('fail', 'Inconceivable!');
+        }
+        return back();
     }
 }
