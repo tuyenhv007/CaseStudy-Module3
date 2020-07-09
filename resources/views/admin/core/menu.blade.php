6f0bda1dc58f1bafcea7d65212e20efa796bf7b3
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-07-09 22:37:17 +0700

    delete layout pages

--- resources/views/admin/core/menu.blade.php
+++ resources/views/admin/core/menu.blade.php
@@ -17,7 +17,8 @@
             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
         </a>
         <div class="collapse" id="collapseLayouts" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-            <nav class="sb-sidenav-menu-nested nav"><a href="{{route('product.add')}}" class="nav-link">Add Product</a><a href="{{route('product.index')}}" class="nav-link">List</a></nav>
+            <nav class="sb-sidenav-menu-nested nav"><a href="{{route('product.add')}}" class="nav-link">Add
+                    Product</a><a href="{{route('product.index')}}" class="nav-link">List</a></nav>
         </div>
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false"
            aria-controls="collapsePages">
@@ -26,40 +27,24 @@
             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
         </a>
         <div class="collapse" id="collapsePages" aria-labelledby="headingThree" data-parent="#sidenavAccordion">
-            <nav class="sb-sidenav-menu-nested nav"><a href="{{ route('bills.index') }}" class="nav-link">List</a>
-
+            <nav class="sb-sidenav-menu-nested nav">
+                <a href="{{ route('bills.index') }}" class="nav-link">List</a>
         </div>
-
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false"
-           aria-controls="collapsePages">
-            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-            Pages
-            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-        </a>
         <div class="collapse" id="collapsePages" aria-labelledby="headingThree" data-parent="#sidenavAccordion">
             <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth"
-                   aria-expanded="false" aria-controls="pagesCollapseAuth">Authentication
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
                 <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
                      data-parent="#sidenavAccordionPages">
-                    <nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="login.html">Login</a><a
+                    <nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="login.html"></a><a
                             class="nav-link" href="register.html">Register</a><a class="nav-link" href="password.html">Forgot
-                            Password</a></nav>
+                        </a></nav>
                 </div>
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError"
-                   aria-expanded="false" aria-controls="pagesCollapseError">Error
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
                 <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
                      data-parent="#sidenavAccordionPages">
                     <nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="401.html">401 Page</a><a
                             class="nav-link" href="404.html">404 Page</a><a class="nav-link" href="500.html">500
-                            Page</a></nav>
+                        </a></nav>
                 </div>
             </nav>
         </div>
-
     </div>
 </div>
